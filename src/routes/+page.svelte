c7ae9805d0d07a95cc5a90dc3a656a4b31247456
selectdev <selectdev01@gmail.com>
2023-12-03 02:00:40 +0000

    Fix logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-white font-bold text-lg">Welcome to SvelteKit</h1>
+<p class="text-white font-bold text-base">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>

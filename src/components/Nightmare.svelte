b2c651031f2019d519d23231b5b8750a1c5bc6f4
selectdev <selectdev01@gmail.com>
2023-12-03 01:57:16 +0000

    design updates

---
+++ src/components/Nightmare.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let Title: string;
+	export let Description: string;
+	export let Image: string = '/logo.png';
+</script>
+
+<svelte:head>
+	<title>{Title}</title>
+	<meta property="og:title" content={Title} />
+	<meta property="og:site_name" content="Popkat CDN" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://popkat.select-list.xyz" />
+	<meta property="og:image" content={Image} />
+	<meta property="og:description" content={Description} />
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
 	import '../app.css';
+	import '@fortawesome/fontawesome-free/css/all.min.css';
+	import 'sweetalert2/dist/sweetalert2.min.css';
+	import Header from '../components/Header.svelte';
+	import Nightmare from '../components/Nightmare.svelte';
+
+	export let data;
 </script>
 
-<slot />
+<Header user={data.user || null} />
+
+<Nightmare
+	Title="Home"
+	Description="This website is extremely experimental, and should not be used by the public at this time."
+/>
+
+<div class="m-4">
+	<slot />
+</div>
